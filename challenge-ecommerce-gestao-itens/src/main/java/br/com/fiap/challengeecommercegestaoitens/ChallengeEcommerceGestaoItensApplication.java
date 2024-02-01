8eb91b86e499a2d497c48691aea81f351a81a27e
Otávio Prado <otavio.prado@sensedia.com>
2024-02-01 08:52:21 -0300

    criação do microserviço de gestão de itens

---
+++ challenge-ecommerce-gestao-itens/src/main/java/br/com/fiap/challengeecommercegestaoitens/ChallengeEcommerceGestaoItensApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengeecommercegestaoitens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeEcommerceGestaoItensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeEcommerceGestaoItensApplication.class, args);
+	}
+
+}
---
+++ challenge-ecommerce-gestao-itens/src/test/java/br/com/fiap/challengeecommercegestaoitens/ChallengeEcommerceGestaoItensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challengeecommercegestaoitens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeEcommerceGestaoItensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
