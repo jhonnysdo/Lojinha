468a7c69af8bd426cba060183df1a31a5422b31c
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-03-09 14:49:53 -0300

    Criando CRUD FormaPagamento

--- challenge-ecommerce-pagamento/src/main/java/br/com/fiap/challengeecommercepagamentos/controllers/FormaPagamentoController.java
+++ challenge-ecommerce-pagamento/src/main/java/br/com/fiap/challengeecommercepagamentos/controllers/FormaPagamentoController.java
@@ -1,15 +1,15 @@
 package br.com.fiap.challengeecommercepagamentos.controllers;
 
-import br.com.fiap.challengeecommercepagamentos.dto.request.FormaPagamentoDTO;
-import br.com.fiap.challengeecommercepagamentos.entity.FormaPagamento;
-import br.com.fiap.challengeecommercepagamentos.repository.FormaPagamentoRepository;
+
+import br.com.fiap.challengeecommercepagamentos.dto.request.formasPagamentosDTO.CartaoDTO;
 import br.com.fiap.challengeecommercepagamentos.services.FormaPagamentoService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @AllArgsConstructor
@@ -21,25 +21,33 @@ public class FormaPagamentoController {
     private final FormaPagamentoService formaPagamentoService;
 
     @PostMapping
-    public ResponseEntity<FormaPagamentoDTO> cadastrarFormaPagamento(@RequestBody FormaPagamentoDTO formaPagamentoDTO) {
-        return null;
+    public ResponseEntity<CartaoDTO> cadastrarCartao(@RequestBody CartaoDTO cartaoDTO) {
+        var cartaoCadastrado = formaPagamentoService.cadastrarCartao(cartaoDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand((cartaoCadastrado.getIdCartao())).toUri();
+        return ResponseEntity.created(uri).body(cartaoCadastrado);
     }
+
 
     @GetMapping
-    public ResponseEntity<List<FormaPagamento>> listarFormasPagamento() {
-     return new ResponseEntity<>(formaPagamentoService.listarFormasPagamento(),HttpStatus.OK);
+    public ResponseEntity<List<CartaoDTO>> listarCartoes(){
+        List<CartaoDTO> cartoes = formaPagamentoService.listarCartoes();
+        return ResponseEntity.ok(cartoes);
+
     }
 
-    @PutMapping
-    public ResponseEntity<FormaPagamento> editarFormaPagamento() {
-        return null;
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CartaoDTO> editarFormaPagamento(@RequestBody CartaoDTO cartaoDTO, @PathVariable String idCartao) {
+        var cartaoAtualizado = formaPagamentoService.editarFormaPagamento(idCartao, cartaoDTO);
+        return ResponseEntity.ok(cartaoAtualizado);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void>removerFormaPagamento(@PathVariable String id) {
-       formaPagamentoService.removerFormaPagamento(id);
-       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Void> removerFormaPagamento(@PathVariable String id) {
+        formaPagamentoService.removerFormaPagamento(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
-}
+    }
 
 }
--- challenge-ecommerce-pagamento/src/main/java/br/com/fiap/challengeecommercepagamentos/services/FormaPagamentoService.java
+++ challenge-ecommerce-pagamento/src/main/java/br/com/fiap/challengeecommercepagamentos/services/FormaPagamentoService.java
@@ -1,37 +1,63 @@
 package br.com.fiap.challengeecommercepagamentos.services;
 
-import br.com.fiap.challengeecommercepagamentos.dto.request.FormaPagamentoDTO;
+import br.com.fiap.challengeecommercepagamentos.dto.request.FormaPagamentoDtoRequest;
+import br.com.fiap.challengeecommercepagamentos.dto.request.formasPagamentosDTO.CartaoDTO;
+import br.com.fiap.challengeecommercepagamentos.entity.Cartao;
 import br.com.fiap.challengeecommercepagamentos.entity.FormaPagamento;
+import br.com.fiap.challengeecommercepagamentos.enums.TipoFormaPagamento;
+import br.com.fiap.challengeecommercepagamentos.mapper.CartaoMapper;
+import br.com.fiap.challengeecommercepagamentos.mapper.FormaPagamentoMapper;
+import br.com.fiap.challengeecommercepagamentos.repository.CartaoRepository;
 import br.com.fiap.challengeecommercepagamentos.repository.FormaPagamentoRepository;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @Service
 @AllArgsConstructor
 public class FormaPagamentoService {
 
-
     private final FormaPagamentoRepository formaPagamentoRepository;
+    private final FormaPagamentoMapper formaPagamentoMapper;
+    private final CartaoMapper cartaoMapper;
+    private final CartaoRepository cartaoRepository;
 
-    public ResponseEntity<FormaPagamentoDTO> cadastrarFormaPagamento(@RequestBody FormaPagamentoDTO formaPagamentoDTO) {
-        return null;
+
+    public CartaoDTO cadastrarCartao(CartaoDTO cartaoDTO) {
+        final Cartao cartaoCadastrado = cartaoRepository.save(cartaoMapper.toEntity(cartaoDTO));
+        return cartaoMapper.toDTO(cartaoCadastrado);
+    }
+
+    public List<CartaoDTO> listarCartoes() {
+    List<Cartao> cartoes = cartaoRepository.findAll();
+    return cartoes.stream().map(cartaoMapper::toDTO)
+            .collect(Collectors.toList());
     }
 
 
-    public List<FormaPagamento> listarFormasPagamento() {
-        return null;
+    public CartaoDTO editarFormaPagamento(String id, CartaoDTO cartaoDTO) {
+        Optional<Cartao> cartaoOptional = cartaoRepository.findById(id);
+        if (cartaoOptional.isPresent()) {
+            Cartao cartao = cartaoOptional.get();
+
+            cartao.setNumeroCartao(cartaoDTO.getNumeroCartao());
+            cartao.setNomeResponsavel(cartaoDTO.getNomeResponsavel());
+            cartao.setDataVencimento(cartaoDTO.getDataVencimento());
+            cartao.setCodigoSeguranca(cartaoDTO.getCodigoSeguranca());
+
+            cartao = cartaoRepository.save(cartao);
+
+            return cartaoMapper.toDTO(cartao);
+        }
+        return new CartaoDTO();
     }
 
-
-    public ResponseEntity<FormaPagamento> editarFormaPagamento() {
-        return null;
-    }
-    
     public void removerFormaPagamento(String id) {
         formaPagamentoRepository.deleteById(id);
     }
