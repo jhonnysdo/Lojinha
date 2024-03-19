5c8cca5ff2b1bf8f62072791be21659d78e1ecc1
Jhonny Santos de Oliveira <rm348972@fiap.com.br>
2024-03-19 17:12:54 -0300

    update

--- challenge-ecommerce-carrinho-de-compras/src/test/java/br/com/fiap/challengeecommercecarrinhodecompras/controllers/CarrinhoControllerTest.java
+++ challenge-ecommerce-carrinho-de-compras/src/test/java/br/com/fiap/challengeecommercecarrinhodecompras/controllers/CarrinhoControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +18,6 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
--- challenge-ecommerce-carrinho-de-compras/src/test/java/br/com/fiap/challengeecommercecarrinhodecompras/services/ProdutoServiceTest.java
+++ challenge-ecommerce-carrinho-de-compras/src/test/java/br/com/fiap/challengeecommercecarrinhodecompras/services/ProdutoServiceTest.java
@@ -1,70 +1,67 @@
-package br.com.fiap.challengeecommercecarrinhodecompras.services;
+package br.com.fiap.challengeecommercecarrinhodecompras.controllers;
 
-import br.com.fiap.challengeecommercecarrinhodecompras.exceptions.ItemNotFoundException;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import br.com.fiap.challengeecommercecarrinhodecompras.dto.ItemCarrinhoDTO;
+import br.com.fiap.challengeecommercecarrinhodecompras.entity.Carrinho;
+import br.com.fiap.challengeecommercecarrinhodecompras.entity.ItemCarrinho;
+import br.com.fiap.challengeecommercecarrinhodecompras.services.CarrinhoService;
+import br.com.fiap.challengeecommercecarrinhodecompras.services.JwtService;
+import br.com.fiap.challengeecommercecarrinhodecompras.services.ProdutoService;
+import br.com.fiap.challengeecommercepagamentos.entity.Pagamento;
+import br.com.fiap.challengeecommercepagamentos.enums.TipoFormaPagamento;
+import br.com.fiap.challengeecommercepagamentos.services.PagamentoService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.*;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import java.util.List;
+@RestController
+@RequestMapping("/carrinho")
+@AllArgsConstructor
+public class CarrinhoController {
 
-class ProdutoServiceTest {
+    private CarrinhoService carrinhoService;
+    private JwtService jwtService;
+    private ProdutoService produtoService;
+    private static PagamentoService pagamentoService;
 
-    private final RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
-    private final ProdutoService produtoService = new ProdutoService(restTemplate);
-
-    @Test
-    void testFetchProdutoSuccess() {
-        Long produtoId = 1L;
-        String authHeader = "testAuthHeader";
-        String testResponse = "testApiResponse";
-        when(restTemplate.exchange(
-                "http://localhost:8081/gestao-itens/itens/" + produtoId,
-                HttpMethod.GET,
-                new HttpEntity<>(createHeaders(authHeader)),
-                String.class))
-                .thenReturn(new ResponseEntity<>(testResponse, HttpStatus.OK));
-
-        ResponseEntity<String> response = produtoService.fetchProduto(produtoId, authHeader);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(testResponse, response.getBody());
+    @GetMapping
+    public List<ItemCarrinho> listarItensCarrinho(@RequestParam String username) {
+        return carrinhoService.listarItensCarrinho(username);
     }
 
-    @Test
-    void testFetchProdutoNotFound() {
-        Long produtoId = 1L;
-        String authHeader = "testAuthHeader";
-        when(restTemplate.exchange(
-                "http://localhost:8081/gestao-itens/itens/" + produtoId,
-                HttpMethod.GET,
-                new HttpEntity<>(createHeaders(authHeader)),
-                String.class))
-                .thenThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
+    @PostMapping
+    public ItemCarrinho adicionarItemCarrinho(@Valid @RequestBody ItemCarrinhoDTO itemCarrinhoDTO, @RequestHeader(value = "Authorization") String authorizationHeader) {
+        var jwtToken = authorizationHeader.substring(7);
 
-        assertThrows(ItemNotFoundException.class, () -> produtoService.fetchProduto(produtoId, authHeader));
+        ItemCarrinho itemCarrinho = ItemCarrinho.builder()
+                .produtoId(itemCarrinhoDTO.getProdutoId())
+                .preco(itemCarrinhoDTO.getPreco())
+                .quantidade(itemCarrinhoDTO.getQuantidade())
+                .username(jwtService.extractUsername(jwtToken))
+                .build();
+
+        ResponseEntity<String> response = produtoService.fetchProduto(itemCarrinho.getProdutoId(), authorizationHeader);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return carrinhoService.adicionarItemCarrinho(itemCarrinho);
+        } else {
+            throw new RuntimeException("Erro ao chamar o serviço de itens.");
+        }
     }
 
-    @Test
-    void testFetchProdutoOtherError() {
-        Long produtoId = 8021L;
-        String authHeader = "testAuthHeader";
-        when(restTemplate.exchange(
-                "http://localhost:8081/gestao-itens/itens/" + produtoId,
-                HttpMethod.GET,
-                new HttpEntity<>(createHeaders(authHeader)),
-                String.class))
-                .thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
-        
-        assertThrows(RuntimeException.class, () -> produtoService.fetchProduto(produtoId, authHeader));
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> removerItemCarrinho(@PathVariable Long id) {
+        carrinhoService.removerItemCarrinho(id);
+        return ResponseEntity.ok("Item removido com sucesso.");
     }
 
-    private HttpHeaders createHeaders(String authHeader) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", authHeader);
-        return headers;
+    @PostMapping("/fecharCompra")
+    public ResponseEntity<String> fecharCarrinho(@RequestParam String username, @RequestParam TipoFormaPagamento tipoFormaPagamento) {
+        return   carrinhoService.fecharCarrinho(username, tipoFormaPagamento);
+    }
+
+    @GetMapping("/buscarCarrinhoPorUsername")
+    public ResponseEntity<List<ItemCarrinho>> buscarCarrinhoPorUsername(@RequestParam String username) {
+        return carrinhoService.buscarCarrinhoPorUsername(username);
     }
 }
