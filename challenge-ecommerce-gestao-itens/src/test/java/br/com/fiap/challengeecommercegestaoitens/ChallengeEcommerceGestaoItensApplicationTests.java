37dfa8d1f5bfc21fcfeda0d7297d48727b566de1
Jhonny Santos de Oliveira <rm348972@fiap.com.br>
2024-03-19 15:02:11 -0300

    Refactor ChallengeEcommerceGestaoItensApplicationTests

--- challenge-ecommerce-gestao-itens/src/test/java/br/com/fiap/challengeecommercegestaoitens/ChallengeEcommerceGestaoItensApplicationTests.java
+++ challenge-ecommerce-gestao-itens/src/test/java/br/com/fiap/challengeecommercegestaoitens/ChallengeEcommerceGestaoItensApplicationTests.java
@@ -3,11 +3,16 @@ package br.com.fiap.challengeecommercegestaoitens;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class ChallengeEcommerceGestaoItensApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testMainMethod() {
+		// Verifica se o método main da aplicação é executado sem lançar exceções
+		assertDoesNotThrow(() -> ChallengeEcommerceGestaoItensApplication.main(new String[]{}));
 	}
 
-}
+	}
+
