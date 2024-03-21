b782415e8102f2a844ab002c69070d43660c564d
Otávio Prado <otavio.prado@sensedia.com>
2024-03-21 09:40:28 -0300

    incluindo ExceptionHandler e ajustando collection postman

--- challenge-ecommerce-login/src/main/java/br/com/fiap/challengeecommercelogin/handlers/CustomExceptionHandler.java
+++ challenge-ecommerce-login/src/main/java/br/com/fiap/challengeecommercelogin/handlers/CustomExceptionHandler.java
@@ -5,8 +5,13 @@ import br.com.fiap.challengeecommercelogin.exceptions.UserAlreadyExistsException
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -22,4 +27,16 @@ public class CustomExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse("Token de autenticação expirado", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        ErrorResponse errorResponse = new ErrorResponse("Erro de validação", errors.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
