e648fe55045aab33e4e2d3a8e09c9a28115aa5f4
Jhonny Santos de Oliveira <rm348972@fiap.com.br>
2024-03-21 20:03:33 -0300

    Create PingControllerTest (API Carrinho)

--- challenge-ecommerce-gestao-itens/src/test/java/br/com/fiap/challengeecommercegestaoitens/services/ItemServiceTest.java
+++ challenge-ecommerce-gestao-itens/src/test/java/br/com/fiap/challengeecommercegestaoitens/services/ItemServiceTest.java
@@ -15,6 +15,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import br.com.fiap.challengeecommercegestaoitens.dto.ItemDTO;
+import br.com.fiap.challengeecommercegestaoitens.entity.Item;
+import br.com.fiap.challengeecommercegestaoitens.exceptions.ItemNotFoundException;
+import br.com.fiap.challengeecommercegestaoitens.repository.ItemRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
 @ExtendWith(MockitoExtension.class)
 class ItemServiceTest {
 
@@ -30,9 +46,11 @@ class ItemServiceTest {
         item.setId(1L);
         when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
 
-        Item result = itemService.buscarPorId(1L);
+        Optional<ItemDTO> result = itemService.buscarPorId(1L);
 
-        assertEquals(1L, result.getId());
+       // Item result = itemService.buscarPorId(1L);
+
+        assertEquals(1L, result.get());
     }
 
     @Test
